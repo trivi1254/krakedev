7f93e54116b467ef59ab74d9df7883da6f2af78c
trivi1254 <johnmichaelloor@gmail.com>
2024-01-23 12:56:48 -0500

    se configuro la funcion para q cambie el gif princ a un gif secun porsupuesto se realizo los cambios tambien en el codigo html

--- ejercicio git/ejerciciogit.js
+++ ejercicio git/ejerciciogit.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpimagensaludo;
+    cmpimagensaludo=document.getElementById("imgsaludo");
+    cmpimagensaludo.src="./imagenes/giphy.gif";
     console.log("esto es una prueba del git");
 }
