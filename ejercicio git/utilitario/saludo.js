9ae0de30ab1ec202daa6bcfd0a87fc7d774c832a
trivi1254 <johnmichaelloor@gmail.com>
2024-01-24 13:13:08 -0500

    SE AGREGARON LAS FUNCIONES DE RECUPERARINT Y RECUPERARFLOAT + 2 CAJA DE TEXTO PARA COLOCAR LOS VALORES SOLICITADOS

--- ejercicio git/utilitario/saludo.js
+++ ejercicio git/utilitario/saludo.js
@@ -1,9 +1,20 @@
 saludar=function(){
     let nombre=recuperartexto('txtnombre');
     let apellido=recuperartexto('txtapellido');
+    let edad=recuperarint('EDAD');
+    let estatura=recuperarfloat('ESTATURA');
+}
+recuperarint=function(idcomponente){
+    let int=recuperartexto(idcomponente);
+    let valorentero=parseInt(int);
+    return valorentero;
 }
 
-
+recuperarfloat=function(idcomponente){
+    let float=recuperartexto(idcomponente);
+    let valorfloat=parseFloat(float);
+    return valorfloat;
+}
 
 
 recuperartexto=function(idcomponente){
