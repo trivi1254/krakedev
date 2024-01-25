3db1017fceb7d577bbe9b2e48699d489a90d0fa1
trivi1254 <johnmichaelloor@gmail.com>
2024-01-25 15:10:03 -0500

    se creo un sistema de funciones para sacar promedio de notas estudiantiles segun los casilleros , todo esto en la carpeta promedio notas

---
+++ Promedionotas/promedionotas.js
@@ -0,0 +1,13 @@
+calcularpromedionotas=function(){
+    let nota1=recuperarTexto("nota1");
+    let nota2=recuperarTexto("nota2");
+    let nota3=recuperarTexto("nota3");
+    let n1=parseFloat(nota1);
+    let n2=parseFloat(nota2);
+    let n3=parseFloat(nota3);
+    let promedio=calcularpromedio(n1,n2,n3);
+    let decimales=promedio.toFixed(2);
+    cambiarTexto("total",decimales);
+
+
+}
---
+++ Promedionotas/serviciospormedionotas.js
@@ -0,0 +1,6 @@
+calcularpromedio=function(n1,n2,n3){
+let sumanotas=n1+n2+n3;
+let promedio=sumanotas/3;
+
+return promedio
+}
