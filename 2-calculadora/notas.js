1263771bdd517063568e98708dd7bc5f870788a3
trivi1254 <johnmichaelloor@gmail.com>
2024-01-31 20:00:14 -0500

    carpeta calculadora nueva

---
+++ 2-calculadora/notas.js
@@ -0,0 +1,19 @@
+calcularPromedio = function (nota1, nota2, nota3) {
+    let promedio;
+    promedio = (nota1 + nota2 + nota3) / 3;
+    return promedio;
+}
+
+calcular = function () {
+    let nota1;
+    let nota2;
+    let nota3;
+    let resultado;
+    let resultadoFormato;
+    nota1 = recuperarFloat("txtNota1");
+    nota2 = recuperarFloat("txtNota2");
+	nota3 = recuperarFloat("txtNota3");
+    resultado = calcularPromedio(nota1, nota2, nota3);
+    resultadoFormato = resultado.toFixed(2);
+    mostrarTexto("lblResultado", resultadoFormato);
+}
